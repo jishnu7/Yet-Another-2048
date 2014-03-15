0b992f2830c54c1f90dd593c4989c66b1cbed135
Jishnu Mohan <jishnu7@gmail.com>
2014-03-15 18:12:10 +0530

    init

---
+++ src/Application.js
@@ -0,0 +1,124 @@
+/* jshint ignore:start */
+import device;
+import ui.View as View;
+import ui.StackView as StackView;
+import ui.TextView as TextView;
+import ui.widget.GridView as GridView;
+import ui.widget.ButtonView as ButtonView;
+/* jshint ignore:end */
+
+exports = Class(GC.Application, function () {
+  this.initUI = function () {
+    var rootView = new StackView({
+      superview: this,
+      layout: "box",
+      backgroundColor: 'white'
+    });
+
+    var game = new View({
+      layout: 'linear',
+      direction: 'vertical',
+      justifyContent: 'space-outside',
+      layoutWidth: '100%',
+      layoutHeight: '100%',
+    });
+
+    var score = new TextView({
+      superview: game,
+      layout: 'box',
+      color: 'red',
+      text: '2048',
+      size: 30,
+      height: 50,
+    });
+
+    var grid = new GridView({
+      superview: game,
+      layout: 'box',
+      centerX: true,
+      backgroundColor: 'red',
+      width: device.width - 10,
+      height: device.width - 10,
+      rows: 4,
+      cols: 4,
+      horizontalMargin: 5,
+      verticalMargin: 5,
+    });
+    //for(var x=0; x<4; x++) {
+      //for(var y=0; y<4; y++) {
+        var cell = new View({
+          superview: grid,
+          backgroundColor: 'black',
+          col: 0,
+          row: 0,
+        });
+      //}
+    //}
+
+    var horz = new View({
+      superview: game,
+      layout: 'linear',
+      justifyContent: 'space-outside',
+      height: 25,
+      layoutWidth: '100%'
+    });
+
+    var move = function(x, y) {
+      var opts = cell._opts,
+        col = opts.col,
+        row = opts.row;
+
+      col += x;
+      col = col < grid.getCols() ? col : grid.getCols()-1;
+      col = col < 0 ? 0 : col;
+
+      row += y;
+      row = row < grid.getRows() ? row : grid.getRows()-1;
+      row = row < 0 ? 0 : row;
+
+      opts.row = row;
+      opts.col = col;
+    };
+
+    var left = new ButtonView({
+      superview: horz,
+      width: 50,
+      height: 25,
+      title: 'left',
+      on: {
+        up: bind(this, move, -1, 0)
+      }
+    });
+    var right = new ButtonView({
+      superview: horz,
+      width: 50,
+      height: 25,
+      title: 'right',
+      on: {
+        up: bind(this, move, 1, 0)
+      }
+    });
+    var up = new ButtonView({
+      superview: horz,
+      width: 50,
+      height: 25,
+      title: 'up',
+      on: {
+        up: bind(this, move, 0, -1)
+      }
+    });
+    var down = new ButtonView({
+      superview: horz,
+      width: 50,
+      height: 25,
+      title: 'down',
+      on: {
+        up: bind(this, move, 0, 1)
+      }
+    });
+
+    rootView.push(game);
+  };
+  
+  this.launchUI = function () {};
+});
