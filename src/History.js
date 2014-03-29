0dd018c6e155ea60531caea7fabd1398e35aa511
Jishnu Mohan <jishnu7@gmail.com>
2014-03-29 18:47:03 +0530

    close app on backbutton

--- src/History.js
+++ src/History.js
@@ -1,17 +1,14 @@
 exports = (function() {
   var history = {},
-    stack = [],
-    count = 0;
+    stack = [];
 
   history.add = function(callback) {
-    count = 0;
     stack.push(callback);
   };
 
   history.release = function() {
     if(stack.length <= 0) {
-      // return false to close the app
-      return (count > 0 ? false: ++count);
+      return false;
     } else {
       stack.pop()();
     }
