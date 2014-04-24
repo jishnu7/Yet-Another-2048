711a09ce10bb9f3cb81aea7ca5595cad0dcdb86e
Jishnu Mohan <jishnu7@gmail.com>
2014-04-24 21:24:16 +0530

    initial layout for stats screen

--- src/Stats.js
+++ src/Stats.js
@@ -1,7 +1,9 @@
 /* jshint ignore:start */
 import ui.View as View;
+import ui.TextView as TextView;
 import src.gc.ButtonView as ButtonView;
 
+import src.Utils as Utils;
 import src.PlayGame as PlayGame;
 /* jshint ignore:end */
 
@@ -11,39 +13,62 @@ exports = Class(View, function(supr) {
     merge(opts, {
       layout: 'linear',
       direction: 'vertical',
-      justifyContent: 'space-outside',
       layoutWidth: '100%',
       layoutHeight: '100%'
     });
     supr(this, 'init', [opts]);
 
-    new ButtonView({
+    new TextView({
       superview: this,
+      layout: 'box',
+      centerX: true,
+      layoutWidth: '90%',
+      height: 50,
+      text: 'Stats',
+      size: 50,
+      color: Utils.colors.text,
+      fontFamily: Utils.fonts.text,
+      horizontalAlign: 'left',
+      top: 50
+    });
+
+    var containerPlay = new View({
+      superview: this,
+      layout: 'linear',
+      justifyContent: 'space',
+      layoutWidth: '90%',
+      height: 80,
+      centerX: true,
+      top: 50
+    });
+
+    new ButtonView({
+      superview: containerPlay,
       centerX: true,
       width: 362,
       height: 75,
-      bottom: 15,
+      scale: 0.9,
       images: {
         up: 'resources/images/btn_achievements.png',
         down: 'resources/images/btn_achievements_down.png'
       },
       on: {
-        up: PlayGame.showLeaderBoard
+        up: PlayGame.showAchievements
       }
     });
 
     new ButtonView({
-      superview: this,
+      superview: containerPlay,
       centerX: true,
       width: 362,
       height: 75,
-      bottom: 15,
+      scale: 0.9,
       images: {
         up: 'resources/images/btn_leaderboard.png',
         down: 'resources/images/btn_leaderboard_down.png'
       },
       on: {
-        up: PlayGame.showAchievements
+        up: PlayGame.showLeaderBoard
       }
     });
   };
